2f0066b6741c5637a272b0c12797fe83708b39b6
Randy Houlahan <randy.houlahan@cbd.int>
2022-09-26 14:51:53 -0400

    dev env and d9

--- src/tasks/reload/sites.mjs
+++ src/tasks/reload/sites.mjs
@@ -8,14 +8,15 @@ import { execSync     }            from 'child_process'
 
 const HB = Handlebars.create()
 
-export const initSites = () =>{
+export const initSites = function (){
+  const isDev = Object.values(arguments).includes('-d')
 
-  const { name, sites } = config
+  const { sites } = config
 
   execSync(`mkdir -p ${sitesCtx}`)
 
   createSettingsCommon()
-  writeFile(sitesCtx, `sites.php`, getSitesPhpTemplate(name, sites))
+  writeFile(sitesCtx, `sites.php`, getSitesPhpTemplate(sites, isDev))
   createSitesDirectories()
 }
 
@@ -42,16 +43,20 @@ function createSitesDirectories(){
   }
 }
 
-function getSitesPhpTemplate(name, sites){
+function getSitesPhpTemplate(sites, isDev){
   let templateString = ''
 
   for (const code in sites) {
-    templateString += `$sites["${code}.chm-cbd.net"] = "${code}";\n`;
-    templateString += `$sites["${code}.test.chm-cbd.net"] = "${code}";\n`;
-    if(sites[code].redirectTo)  templateString += `$sites["${sites[code].redirectTo}"] = "${code}";\n`;
-    if(sites[code].urls)
+    if(!isDev) templateString += `$sites["${code}.chm-cbd.net"] = "${code}";\n`;
+    if(!isDev) templateString += `$sites["${code}.test.chm-cbd.net"] = "${code}";\n`;
+
+    if(sites[code].redirectTo && !isDev)  templateString += `$sites["${sites[code].redirectTo}"] = "${code}";\n`;
+
+    if(sites[code].urls && !isDev)
       for (const baseUrl of sites[code].urls) 
         templateString += `$sites["${baseUrl}"] = "${code}";\n`
+
+    if(isDev) templateString += `$sites["${code}.cbddev.xyz"] = "${code}";\n`;
 
     templateString += `\n`
   }
