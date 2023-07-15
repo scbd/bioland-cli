cb44276f3e0fefa2ccec542578b481e3e50da4e2
Randy Houlahan <randy.houlahan@cbd.int>
2023-07-15 07:53:21 -0400

    fix national targets

--- src/util/bioland/migrations/util.mjs
+++ src/util/bioland/migrations/util.mjs
@@ -3,12 +3,24 @@ import   consola from 'consola'
 
 
 export async function getChmDocuments(country, documentType, textSearch){
+    if(documentType === 'nationalTarget') return getNationalTargets(country)
     const isMultipleCountries = Array.isArray(country)
     const countryQuery        = !isMultipleCountries? `+AND+(hostGovernments_ss:${country})` : generateMultipleCountryQuery(country)
     const textQuery           = textSearch? `+AND+text_EN_txt:"!${textSearch}*"` : `+*:*+NOT+text_EN_txt:NBSAP`
     const res                 = await request.get(`https://chm.cbd.int/api/v2013/index/select?q=NOT+version_s:*+AND+realm_ss:chm+AND+schema_s:*++AND+(schema_s:${documentType})${countryQuery}${textQuery}&rows=1000&sort=createdDate_dt+desc&start=0&wt=json`)
 
+
     return res?._body?.response?.docs
+}
+
+async function getNationalTargets(country){
+  const isMultipleCountries = Array.isArray(country)
+  const countryQuery        = !isMultipleCountries? `+AND+(hostGovernments_ss:${country})` : generateMultipleCountryQuery(country)
+
+  const res                 = await request.get(`https://chm.cbd.int/api/v2013/index/select?q=NOT+version_s:*+AND+realm_ss:chm+AND+schema_s:*++AND+(schema_s:nationalTarget)${countryQuery}&rows=25&sort=createdDate_dt+desc&start=0&wt=json`)
+
+
+  return res?._body?.response?.docs
 }
 
 function generateMultipleCountryQuery(countryCodes){
